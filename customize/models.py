336eb872a64088b65d193ab22ad869085d3a0c55
Felix Peña <felixpenagrass@hotmail.com>
2022-07-02 16:36:07 -0400

    cambio menor texto descriptivo app customize admin

--- customize/models.py
+++ customize/models.py
@@ -11,4 +11,4 @@ class Customize(models.Model):
     text_footer = models.CharField(max_length=200, blank=True, null=True)
     logo = models.ImageField(upload_to='logo', default='') #asigna una foto al perfil de usuario un "avatar" clase 24
     def __str__(self):
-        return f"Titulo1: {self.title1} - Titulo2: {self.title2} - Texto footer: {self.text_footer} - Logo: {self.logo.url}"
+        return "Personalice su sitio, no agregue otro objeto porque se va a romper.   "
